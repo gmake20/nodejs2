c8d84403fa1c8e8c4fb22386ba773efd10ae8503
andy <scym14@gmail.com>
2024-05-05 22:41:00 +0900

    nextJS : 메모리에 저장하고 조회하는 API

--- blog/src/blog.controller.ts
+++ blog/src/blog.controller.ts
@@ -33,7 +33,7 @@ export class BlogController {
     }
 
     @Get("/:id") // 6 GET에 URL 매개변수에 id가 있는 요청 처리
-    async getPost(@Param("id") id: string) {
+    getPost(@Param("id") id: string) {
         console.log(`[id: ${id}]게시글 하나 가져오기`);
         const post = await this.blogService.getPost(id);
         console.log(post);
